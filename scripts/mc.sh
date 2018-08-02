bb643c85c0b7a49c489330cf447f6b6f6fcd5600
Malik BEN KIRANE <free.malikbenkirane@gmail.com>
2018-08-02 14:17:22 +0200

    add scripts/mc.sh
    see https://github.com/malikbenkirane/mcee

---
+++ scripts/mc.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+
+PREFIX=~/recess/devel/mcee
+
+MC_PYTHON=$PREFIX/venv/bin/python
+MC_SCLI=$PREFIX/scli.py
+mc="$MC_PYTHON $MC_SCLI"
+
+# launch mcee default action in less, if no args
+if test "$#" -lt 1
+then
+	eval $mc | less
+fi
+
+# otherwise, lauch mcee and pass all args
+eval $mc  $@
