6c1b72166e60e1cfb7b279ba4e6bc8bf762d0e9e
Malik BEN KIRANE <free.malikbenkirane@gmail.com>
2018-08-11 20:22:25 +0200

    update scripts/fzh.sh
    now has an usage message
    now uses fzf rather than fzy
    can take either -h to display the message
    or -y to ask with fzy

--- scripts/fzh.sh
+++ scripts/fzh.sh
@@ -1,8 +1,25 @@
-# path requirements: fzy, bash
+# requires: fzf (or fzy), bash
 # fzh.sh - fuzzy history
+# 
+# I recommend using fzf rather than fzy
+#
 #!/bin/bash -e
 
-COMMAND=$(history | sort -r | fzy | awk '{print $1}')
+usage()
+{
+	cat <<-EOF
+	fzh - fuzzy history
+	usage: fzh [-h|-y]
+	  -y uses fzy rather that fzh
+	  -h print this message and exit
+	EOF
+	exit 2
+}
+
+test -n "$1" -a "$1" = "-h" && usage
+test -n "$1" -a "$1" = "-y" && FZ_CMD=fzy || FZ_CMD=fzf
+
+COMMAND=$(history | sort -r | fzf | awk '{print $1}')
 
 if test -n "$COMMAND"
 then
