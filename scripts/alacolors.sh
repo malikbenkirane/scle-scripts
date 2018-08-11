c7b56690a6ae72a7d4ebb31a24dc3d0a2a101a05
Malik BEN KIRANE <free.malikbenkirane@gmail.com>
2018-08-11 11:55:27 +0200

    update scripts/alacolors.sh
    now leaves a comment about the colorscheme applied in ALACRITTY_CONFIG
    the code is also well more documented

--- scripts/alacolors.sh
+++ scripts/alacolors.sh
@@ -1,19 +1,22 @@
 # requires: bash, alacritty, b16
 #!/bin/bash -e
 
-# the script update ALACRITTY_CONFIG with colors within COLORSCHEME
-# and COLORSCHEME is the absolute path to the Xresources file
+# Configuration
+
+# 1) the script update ALACRITTY_CONFIG with colors within COLORSCHEME_PATH
+# and COLORSCHEME_PATH is the absolute path to the Xresources file
+# 2) the script requires b16 to share the same configuration (cf. b16.sh)
 ALACRITTY_CONFIG=~/.alacritty.yml
 # TODO add support for other colorschemes
 # start with renaming COLORSDIR with COLORSDIRS 
 COLORSDIR=~/ports/base16-xresources/xresources
-COLORSCHEME=${COLORSDIR}/base16-$(b16).Xresources
+COLORSCHEME=base16-$(b16)
+COLORSCHEME_PATH=${COLORSDIR}/${COLORSCHEME}.Xresources
 
-# XXX
-# only compatible with base16 colorschemes
+# XXX only compatible with base16 colorschemes
 # VIMCOLORS=~/.vimcolors
 # if [ -n "$VIMCONFIG" ]; then
-# 	sed -n "/\s*[^"]colo $(basename $COLORSCHEME .Xresources)/" $VIMCOLORS
+# 	sed -n "/\s*[^"]colo $(basename $COLORSCHEME_PATH .Xresources)/" $VIMCOLORS
 
 colors=(black red green yellow blue magenta cyan white)
 
@@ -42,9 +45,10 @@ get_colors()
 	done  
 }
 
-# base16 colors
+# apply "base16" termcolors
+
 color_style=normal
-get_colors ${COLORSCHEME} | while read -r color_values; do
+get_colors ${COLORSCHEME_PATH} | while read -r color_values; do
 	carr=($color_values)
 	if [ "$color_style" = "bright" ]; then
 		filter='/^  # Bright colors/,/^$/'
@@ -61,12 +65,18 @@ get_colors ${COLORSCHEME} | while read -r color_values; do
 done
 
 # background and foreground colors
+
 filter='/^colors:/,/^$/'
+
 # TODO refacto: avoid running cpp multiple times (already ran once in get_colors)
-# bg_color=$(cpp ${COLORSCHEME} | grep background | sed 's/background:\s\+//')
-bg_color=$(cpp ${COLORSCHEME} | grep background | sed 's/\*background:\s\+//')
-fg_color=$(cpp ${COLORSCHEME} | grep foreground | sed 's/\*foreground:\s\+//')
+bg_color=$(cpp ${COLORSCHEME_PATH} | grep background | sed 's/\*background:\s\+//')
+fg_color=$(cpp ${COLORSCHEME_PATH} | grep foreground | sed 's/\*foreground:\s\+//')
 sed -i "${filter}s/\(background:\s\+'\).*/\1${bg_color}'/" $ALACRITTY_CONFIG 
 sed -i "${filter}s/\(foreground:\s\+'\).*/\1${fg_color}'/" $ALACRITTY_CONFIG
 
-echo alacolors: set colorschme: $COLORSCHEME
+# leave with a message in ALACRITTY_CONFIG and to the user
+
+message="# ${COLORSCHEME} (colorscheme applied with scle/scripts/alacolors.sh)"
+
+sed -i "${filter}s/^colors:/colors: ${message//\//\\\/}/" $ALACRITTY_CONFIG
+echo $message
